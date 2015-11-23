1e8b993680746f771358f2ab26c47562e7f0bdcd
Nadreasky <nadreasky@gmail.com>
2015-11-23 22:46:46 +0700

    PC&RoleRepo
    :|

---
+++ NetCafeWeb/Models/PCRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetCafeWeb.Models
+{
+    public class PCRepository : IRepository<PC>
+    {
+        NetCafeEntities1 _netCafeContext;
+        public PCRepository()
+        {
+            _netCafeContext = new NetCafeEntities1();
+        }
+        public IEnumerable<PC> List
+        {
+            get
+            {
+                return _netCafeContext.PCs;
+            }
+        }
+
+        public void Add(PC entity)
+        {
+            _netCafeContext.PCs.Add(entity);
+            _netCafeContext.SaveChanges();
+        }
+
+        public void Delete(PC entity)
+        {
+            _netCafeContext.PCs.Remove(entity);
+            _netCafeContext.SaveChanges();
+        }
+
+        public PC findById(int Id)
+        {
+            var query = (from r in _netCafeContext.PCs where r.PCID == Id select r).FirstOrDefault();
+            return query;
+        }
+
+        public void Update(PC entity)
+        {
+            _netCafeContext.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+            _netCafeContext.SaveChanges();
+        }
+    }
+}
---
+++ NetCafeWeb/Models/RoleRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetCafeWeb.Models
+{
+    public class RoleRepository : IRepository<Role>
+    {
+        NetCafeEntities1 _netCafeContext;
+        public RoleRepository()
+        {
+            _netCafeContext = new NetCafeEntities1();
+        }
+        public IEnumerable<Role> List
+        {
+            get
+            {
+                return _netCafeContext.Roles;
+            }
+        }
+
+        public void Add(Role entity)
+        {
+            _netCafeContext.Roles.Add(entity);
+            _netCafeContext.SaveChanges();
+        }
+
+        public void Delete(Role entity)
+        {
+            _netCafeContext.Roles.Remove(entity);
+            _netCafeContext.SaveChanges();
+        }
+
+        public Role findById(int Id)
+        {
+            var query = (from r in _netCafeContext.Roles where r.RoleID == Id select r).FirstOrDefault();
+            return query;
+        }
+
+        public void Update(Role entity)
+        {
+            _netCafeContext.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+            _netCafeContext.SaveChanges();
+        }
+    }
+}
