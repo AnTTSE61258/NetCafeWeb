6a60ab9aced8931f14dcb47bb3789869d290cd00
MaskiMisu <i.belong.to.wind@gmail.com>
2015-11-23 23:19:40 +0700

    OrderUserRepo

---
+++ NetCafeWeb/Models/OrderRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetCafeWeb.Models
+{
+    public class OrderRepository : IRepository<Order>
+    {
+        NetCafeEntities1 _orderContext;
+        public OrderRepository()
+        {
+            _orderContext = new NetCafeEntities1();
+        }
+        public IEnumerable<Order> List
+        {
+            get
+            {
+                return _orderContext.Orders;
+            }
+        }
+
+        public void Add(Order entity)
+        {
+            _orderContext.Orders.Add(entity);
+            _orderContext.SaveChanges();
+        }
+
+        public void Delete(Order entity)
+        {
+            _orderContext.Orders.Remove(entity);
+            _orderContext.SaveChanges();
+        }
+
+        public Order findById(int Id)
+        {
+            var query = (from r in _orderContext.Orders where r.OrderID == Id select r).FirstOrDefault();
+            return query;
+        }
+
+        public void Update(Order entity)
+        {
+            _orderContext.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+            _orderContext.SaveChanges();
+        }
+    }
+}
---
+++ NetCafeWeb/Models/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetCafeWeb.Models
+{
+    public class UserRepository : IRepository<User>
+    {
+        NetCafeEntities1 _userContext;
+        public UserRepository()
+        {
+            _userContext = new NetCafeEntities1();
+        }
+        public IEnumerable<User> List
+        {
+            get
+            {
+                return _userContext.Users;
+            }
+        }
+
+        public void Add(User entity)
+        {
+            _userContext.Users.Add(entity);
+            _userContext.SaveChanges();
+        }
+
+        public void Delete(User entity)
+        {
+            _userContext.Users.Remove(entity);
+            _userContext.SaveChanges();
+        }
+
+        public User findById(int Id)
+        {
+            var query = (from r in _userContext.Users where r.UserID == Id select r).FirstOrDefault();
+            return query;
+        }
+
+        public void Update(User entity)
+        {
+            _userContext.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+            _userContext.SaveChanges();
+        }
+    }
+}
