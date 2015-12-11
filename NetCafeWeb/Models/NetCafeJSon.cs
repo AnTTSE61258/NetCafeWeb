72f96ff8c47e97d3455648dcd72804bfc1ef8388
AnTTSE61258 <tuanan94@hotmail.com>
2015-12-11 08:09:37 +0700

    FUcking last last

---
+++ NetCafeWeb/Models/NetCafeJSon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetCafeWeb.Models
+{
+    public class NetCafeJSon
+    {
+        public int id { get; set;}
+        public String name { get; set; }
+    }
+}
